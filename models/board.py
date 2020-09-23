acacec964b5c99229f3e93d9b7072c0ecf940d5c
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-09-23 17:00:36 +0200

    Add BoardSchema for serialization

--- models/board.py
+++ models/board.py
@@ -1,4 +1,4 @@
-from db import db
+from db import db, ma
 
 
 class Board(db.Model):
@@ -6,7 +6,9 @@ class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     nb_part = db.Column(db.Integer, nullable=False)
-    parts = db.relationship('Part', backref='board')
+    parts = db.relationship('Part', cascade="all, delete", backref='board')
 
-    def __repr__(self):
-        return f'{self.name} with {self.nb_part} part(s).'
+
+class BoardSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Board
