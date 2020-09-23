fbe5a4fe0102588188b80fb78592b721e35ffc36
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-09-23 16:59:18 +0200

    Add marshmallow to serialyze object

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
+ma = Marshmallow()
 
 
 def init_db(app):
     db.init_app(app)
+    ma.init_app(app)
     db.create_all()
 
     return db
