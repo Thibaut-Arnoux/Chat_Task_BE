456e300168cd912f6ae570ea12f00832735ec5b8
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-09-23 16:59:42 +0200

    Add some routes for Board operations

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_restful import Api
 from db import init_db
+from routes.board import BoardAPI, BoardIdApi
 from models import *
 
 
@@ -9,9 +10,13 @@ def create_app():
     app = Flask(__name__)
     app.config.from_envvar('ENV_FILE_LOCATION')
 
-    api = Api(app)
     with app.app_context():
         init_db(app)
+    api = Api(app)
+
+    # Board endpoint
+    api.add_resource(BoardAPI, '/api/board', endpoint='board')
+    api.add_resource(BoardIdApi, '/api/board/<int:id>', endpoint='board_id')
     return app
 
 
