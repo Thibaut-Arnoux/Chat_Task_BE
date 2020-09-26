8b081d9550fc86856974760ff650cc2399efc577
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-09-26 21:34:45 +0200

    Add new field tag

--- models/part.py
+++ models/part.py
@@ -1,16 +1,10 @@
-from db import db, ma
+from db import db
 
 
 class Part(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
+    tag = db.Column(db.String(10), nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
     messages = db.relationship('Message', backref='part')
-
-
-class PartSchema(ma.SQLAlchemyAutoSchema):
-
-    class Meta:
-        model = Part
-        include_fk = True
